820640e59fab4d4d4bd20395e04bd06585d208f4
Shio703 <shiodjamurashvili26@gmail.com>
2023-12-18 20:48:24 +0400

    Category & DarkMode

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./navbar.module.css";
 import Link from "next/link";
+import DarkModeToggle from "../darkModeToggle/DarkModeToggle";
 
 function Navbar() {
   const links = [
@@ -43,6 +44,7 @@ function Navbar() {
         Lamamia
       </Link>
       <div className={styles.links}>
+        <DarkModeToggle/>
         {links.map((link) => (
           <Link href={link.url}>{link.title}</Link>
         ))}
